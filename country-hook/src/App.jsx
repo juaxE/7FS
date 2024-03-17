f4b85c10ef05756b56d387f77e5b05d5f5a73acd
juaxE <juho.kirvesmaki@helsinki.fi>
2024-03-17 15:41:40 +0200

    7.7

---
+++ country-hook/src/App.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      if (name) {
+        try {
+          const newCountry = await axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+          setCountry(newCountry)
+        }
+        catch {
+          setCountry({ notFound: true })
+        }
+      }
+      else {
+        setCountry(null)
+      }
+    }
+
+    fetchCountry()
+  }, [name])
+
+  console.log(country)
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (country.notFound) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div>
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`} />
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
